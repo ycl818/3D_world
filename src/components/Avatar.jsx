f81bbf02f57e28af37f55866a879b570db02a62c
yichien <kevinyichienlee@gmail.com>
2024-10-03 22:44:48 +0800

    add simple animation

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,12 +1,18 @@
-import { useGLTF } from "@react-three/drei";
-import { Suspense, useRef } from "react";
+import { useAnimations, useFBX, useGLTF } from "@react-three/drei";
+import { Suspense, useEffect, useRef } from "react";
 import { pb, useCongfiguratorStore } from "../store";
 import Asset from "./Asset";
 
 const Avatar = ({ ...props }) => {
   const group = useRef();
   const { nodes } = useGLTF("/models/Armature.glb");
+  const { animations } = useFBX("/models/Idle.fbx");
   const customization = useCongfiguratorStore((state) => state.customization);
+  const { actions } = useAnimations(animations, group);
+
+  useEffect(() => {
+    actions["mixamo.com"]?.play();
+  }, [actions]);
 
   return (
     <group ref={group} {...props} dispose={null}>
