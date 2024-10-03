36d2c6e3d3ed2019096c498e43983f3215ba7372
yichien <kevinyichienlee@gmail.com>
2024-10-03 22:31:13 +0800

    add lights and shadow

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,16 @@ function App() {
       <UI />
       <Canvas
         camera={{
-          position: [3, 3, 3],
+          position: [-1, 1, 5],
+          fov: 45,
         }}
+        shadows
       >
-        <color attach="background" args={["#333333"]} />
-
-        <Experience />
+        <color attach="background" args={["#555"]} />
+        <fog attach="fog" args={["#555", 15, 25]} />
+        <group position-y={-1}>
+          <Experience />
+        </group>
       </Canvas>
     </>
   );
